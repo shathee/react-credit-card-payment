9ba389c7abd0c268cdfa083fff558dacdf478c23
Baki <sat.sust@gmail.com>
2021-05-01 18:38:15 +0200

    styles added for preliminaryy components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import CardForm from './components/CardForm';
+import Card from './components/Card';
 
 import './app.css';
 
 function App() {
   return (
     <div className="App">
+      <Card />
       <CardForm />
     </div>
   );
