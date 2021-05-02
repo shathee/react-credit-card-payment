25f8771041bbe16b20b54f2beaf0834ff66242da
Baki <sat.sust@gmail.com>
2021-05-02 22:31:04 +0200

    form elements design done

--- src/components/cardForm/CardForm.js
+++ src/components/cardForm/CardForm.js
@@ -7,25 +7,39 @@ export default function CardForm({setFlipStatus, flipStatus}) {
     }
     return (
         <div className={styles.CardForm}>
-            {/* <input type="text" onFocus={ () => changeFlipStatus (false) } TestClick />
-            <input type="text" onFocus={ () => changeFlipStatus (true) } TestClick /> */}
                 <div className={styles.FormContainer}>
-                    <div class="field-container">
+                    <div className={styles.FieldGroup}>
                         <label for="name">Name</label>
                         <input id="name" maxlength="20" type="text" />
                     </div>
-                    <div class="field-container">
-                        <label for="cardnumber">Card Number</label><span id="generatecard">generate random</span>
+                    <div className={styles.FieldGroup}>
+                        <label for="cardnumber">Card Number</label>
                         <input id="cardnumber" type="text" pattern="[0-9]*" inputmode="numeric" />
                     
                     </div>
-                    <div class="field-container">
-                        <label for="expirationdate">Expiration (mm/yy)</label>
-                        <input id="expirationdate" type="text" pattern="[0-9]*" inputmode="numeric" />
+                    <div className={styles.ExpAndSec}>
+                        <div className={styles.FieldGroup}>
+                            <label for="expirationdate">Expiration (mm/yy)</label>
+                            <select>
+                                <option>Month</option>
+                                <option></option>
+                                <option></option>
+                            </select>
+                            <select>
+                                <option>Year</option>
+                                <option></option>
+                                <option></option>
+                            </select>
+                        </div>
+                        <div className={[styles.FieldGroup, styles.Sec].join(' ')}>
+                            <label for="securitycode">CVV</label>
+                            <input id="securitycode" type="text" pattern="[0-9]*" inputmode="numeric"
+                            onFocus={ () => changeFlipStatus (true) }
+                            onBlur={ () => changeFlipStatus (false) } />
+                        </div>
                     </div>
-                    <div class="field-container">
-                        <label for="securitycode">Security Code</label>
-                        <input id="securitycode" type="text" pattern="[0-9]*" inputmode="numeric" />
+                    <div className={styles.FieldGroup}>
+                        <input type="submit" value="Submit" />
                     </div>
                 </div>
         </div>
