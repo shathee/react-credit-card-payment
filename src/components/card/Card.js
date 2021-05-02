2c4182b176e231cad3c50a66c00d0cf5866f5894
Baki <sat.sust@gmail.com>
2021-05-02 20:52:57 +0200

    form elements added

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -12,9 +12,9 @@ import styles from './Card.module.css'
 
 
 export default function Card({flipStatus}) {
-    console.log(flipStatus)
+    
     const flipClass = flipStatus ? styles.Flipped : ''
-    console.log(flipClass)
+    
     return (
         <div className={[styles.Card, flipClass ].join(' ')}>
             <div className={styles.Front}>
@@ -30,21 +30,6 @@ export default function Card({flipStatus}) {
             <CardBackWhiteLine />
             <ProviderNameBack />
             </div>
-            
-            
-            {/* <div class="data">
-                <div class="pan" title="4123 4567 8910 1112">4123 4567 8910 1112</div>
-                <div class="first-digits">4123</div>
-                <div class="exp-date-wrapper">
-                    <div class="left-label">EXPIRES END</div>
-                    <div class="exp-date">
-                        <div class="upper-labels">MONTH/YEAR</div>
-                        <div class="date" title="01/17">01/17</div>
-                    </div>
-                </div>
-                <div class="name-on-card" title="John Doe">John Doe</div>
-            </div> */}
-                 
         </div>
     )
 }
--- src/components/cardForm/CardForm.js
+++ src/components/cardForm/CardForm.js
@@ -2,14 +2,32 @@ import React from 'react';
 import styles from './CardForm.module.css'
 
 export default function CardForm({setFlipStatus, flipStatus}) {
-    const btnClicked = (s) => {
+    const changeFlipStatus = (s) => {
         setFlipStatus(s)
     }
     return (
         <div className={styles.CardForm}>
-
-            <input type="text" onFocus={ () => btnClicked (false) } TestClick />
-            <input type="text" onFocus={ () => btnClicked (true) } TestClick />
+            {/* <input type="text" onFocus={ () => changeFlipStatus (false) } TestClick />
+            <input type="text" onFocus={ () => changeFlipStatus (true) } TestClick /> */}
+                <div className={styles.FormContainer}>
+                    <div class="field-container">
+                        <label for="name">Name</label>
+                        <input id="name" maxlength="20" type="text" />
+                    </div>
+                    <div class="field-container">
+                        <label for="cardnumber">Card Number</label><span id="generatecard">generate random</span>
+                        <input id="cardnumber" type="text" pattern="[0-9]*" inputmode="numeric" />
+                    
+                    </div>
+                    <div class="field-container">
+                        <label for="expirationdate">Expiration (mm/yy)</label>
+                        <input id="expirationdate" type="text" pattern="[0-9]*" inputmode="numeric" />
+                    </div>
+                    <div class="field-container">
+                        <label for="securitycode">Security Code</label>
+                        <input id="securitycode" type="text" pattern="[0-9]*" inputmode="numeric" />
+                    </div>
+                </div>
         </div>
     )
 }
